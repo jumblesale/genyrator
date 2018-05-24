3ccce3b0997b4592a2acb448743d883a18a7415f
jumblesale <rob@robsollars.co.uk>
2018-05-24 07:37:18 +0100

    started tracking setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='genyrator',
+    version='0.0.2',
+    packages=['genyrator'],
+    url='https://github.com/jumblesale/genyrator',
+    license='',
+    author='jumblesale',
+    author_email='',
+    description=''
+)
