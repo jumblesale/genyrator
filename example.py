f652bdd4942cbe758975593d4ff2cff3c6b68de8
jumblesale <rob@robsollars.co.uk>
2018-05-24 07:36:52 +0100

    moved template generation to classes

---
+++ example.py
@@ -0,0 +1,40 @@
+from genyrator.genyrator import (
+    render_db_model, JoinOption, create_relationship, create_entity_from_exemplar,
+    render_type_model,
+    render_type_constructor)
+import json
+
+example_entity = {
+    "class_name": 'Dog',
+    "columns": [
+        {"name": "name", "type": "string"}, {"name": "age", "type": "int"}
+    ]
+}
+
+example_from_exemplar = """
+{
+    "name": "Charles",
+    "age": 4,
+    "goodness": 11.6,
+    "dob": "2013-10-17T00:00",
+    "ownerId": 1
+}
+"""
+
+if __name__ == '__main__':
+    dog_entity = create_entity_from_exemplar(
+        class_name='Dog',
+        exemplar=json.loads(example_from_exemplar),
+        foreign_keys=[('ownerId', 'owner.owner_id')],
+        indexes=['name'],
+    )
+    dog_entity.relationships = [create_relationship('Owner', True, False, JoinOption.to_one, 'owner_dogs')]
+    print(dog_entity)
+    print(render_db_model(
+        entity=dog_entity,
+        db_import="from genyrator import db",
+        types_module="doggos"
+    ))
+    print(render_db_model(entity=dog_entity, db_import="from doggos import db", types_module="doggos"))
+    print(render_type_model(entity=dog_entity))
+    print(render_type_constructor(entity=dog_entity))
--- genyrator/genyrator.py
+++ genyrator/genyrator.py
@@ -1,12 +1,8 @@
-import json
 from enum import Enum
 from typing import List, Optional, Dict, Tuple
 import attr
 import re
-
 import inflection
-from jinja2 import Environment, BaseLoader
-
 
 datetime_regex = re.compile('\d{4}-\d{2}-\d{2}T(\d{2}:)*')
 
@@ -38,12 +34,12 @@ class SqlAlchemyTypeOption(Enum):
     datetime = 'Datetime'
 
 
-def _type_option_to_sql_alchemy_type(type_option: TypeOption) -> SqlAlchemyTypeOption:
+def _type_option_to_sqlalchemy_type(type_option: TypeOption) -> SqlAlchemyTypeOption:
     return {
         TypeOption.string:   SqlAlchemyTypeOption.string,
         TypeOption.int:      SqlAlchemyTypeOption.int,
         TypeOption.float:    SqlAlchemyTypeOption.float,
-        TypeOption.datetime: SqlAlchemyTypeOption.datetime
+        TypeOption.datetime: SqlAlchemyTypeOption.datetime,
     }[type_option]
 
 
@@ -63,7 +59,7 @@ def _type_option_to_python_type(type_option: TypeOption) -> PythonTypeOption:
         TypeOption.int:      PythonTypeOption.int,
         TypeOption.dict:     PythonTypeOption.dict,
         TypeOption.list:     PythonTypeOption.list,
-        TypeOption.datetime: PythonTypeOption.datetime
+        TypeOption.datetime: PythonTypeOption.datetime,
     }[type_option]
 
 
@@ -78,26 +74,77 @@ def _type_option_to_default_value(type_option: TypeOption) -> str:
     }[type_option]
 
 
+def _create_padding(padding: int, column_name: str) -> str:
+    return ' ' * (padding - len(column_name))
+
+
 @attr.s
 class Column(object):
-    name:             str =                  attr.ib()
-    camel_case_name:  str =                  attr.ib()
-    sql_alchemy_type: SqlAlchemyTypeOption = attr.ib()
-    python_type:      PythonTypeOption =     attr.ib()
-    default:          str =                  attr.ib()
+    name:            str =                  attr.ib()
+    camel_case_name: str =                  attr.ib()
+    sqlalchemy_type: SqlAlchemyTypeOption = attr.ib()
+    python_type:     PythonTypeOption =     attr.ib()
+    default:         str =                  attr.ib()
+    index:           bool =                 attr.ib()
 
-    def to_dict(self):
+    def to_dict(self) -> Dict:
         return self.__dict__
 
+    def to_sqlalchemy_model_string(self, padding: int, foreign_key_relationship: Optional[str] = None) -> str:
+        if foreign_key_relationship is not None:
+            foreign_key = ", db.ForeignKey('{relationship}".format(relationship=foreign_key_relationship)
+        else:
+            foreign_key = ''
+        return '{column_name} ={spacing} db.Column(db.{sql_type}{fk}{index})'.format(
+            column_name=self.name, sql_type=self.sqlalchemy_type.value,
+            spacing=_create_padding(padding, self.name), fk=foreign_key,
+            index=', index=True' if self.index else '',
+        )
 
-def create_column(name: str, type_option: TypeOption) -> Column:
-    return Column(
-        name=_camel_case_to_snek_case(name),
-        camel_case_name=_snek_case_to_camel_case(name),
-        sql_alchemy_type=_type_option_to_sql_alchemy_type(type_option),
-        python_type=_type_option_to_python_type(type_option),
-        default=_type_option_to_default_value(type_option),
-    )
+    def to_type_constructor_argument_string(self, padding: int) -> str:
+        return '{name}: {spacing}Optional[{type}]=None,'.format(
+            name=self.name, spacing=_create_padding(padding, self.name),
+            type=self.python_type.value,
+        )
+
+    def to_type_constructor_body_string(self) -> str:
+        return '{name}={name} if {name} else {default},'.format(
+            name=self.name, default=self.default,
+        )
+
+    def to_sqlalchemy_type_constructor_string(self) -> str:
+        return '{name}={type}(self.{name}),'.format(
+            name=self.name, type=self.python_type.value,
+        )
+
+    def to_named_tuple_string(self, padding: int):
+        return "        ('{name}', {spacing}{type}),".format(
+            name=self.name, spacing=_create_padding(padding, self.name),
+            type=self.python_type.value,
+        )
+
+
+@attr.s
+class ForeignKey(Column):
+    relationship: str = attr.ib()
+
+    def to_sqlalchemy_model_string(self, padding: int, foreign_key_relationship: Optional[str] = None):
+        return super().to_sqlalchemy_model_string(padding, self.relationship)
+
+
+def create_column(name: str, type_option: TypeOption, foreign_key_relationship: Optional[str] = None, index=False) -> Column:
+    constructor = Column if foreign_key_relationship is None else ForeignKey
+    args = {
+        "name":            _camel_case_to_snek_case(name),
+        "camel_case_name": _snek_case_to_camel_case(name),
+        "sqlalchemy_type": _type_option_to_sqlalchemy_type(type_option),
+        "python_type":     _type_option_to_python_type(type_option),
+        "default":         _type_option_to_default_value(type_option),
+        "index":           index,
+    }
+    if foreign_key_relationship is not None:
+        args["relationship"] = foreign_key_relationship
+    return constructor(**args)
 
 
 def _is_string_date_time_ish(potential_datetime: str) -> bool:
@@ -109,6 +156,7 @@ def _column_from_dict(column_dict: Dict) -> Column:
     return create_column(
         name=column_dict['name'],
         type_option=type_option,
+        foreign_key_relationship=None
     )
 
 
@@ -119,14 +167,48 @@ class JoinOption(Enum):
 
 @attr.s
 class Relationship(object):
-    target_entity_name: str =           attr.ib()
-    target_entity_name_snek_case: str = attr.ib()
-    nullable: bool =                    attr.ib()
-    lazy: bool =                        attr.ib()
-    join: JoinOption =                  attr.ib()
+    target_entity_name:           str =        attr.ib()
+    target_entity_name_snek_case: str =        attr.ib()
+    nullable:                     bool =       attr.ib()
+    lazy:                         bool =       attr.ib()
+    join:                         JoinOption = attr.ib()
+    join_table:                   str =        attr.ib()
 
     def to_dict(self):
         return self.__dict__
+
+    def to_type_constructor_argument_string(self, padding: int) -> str:
+        return '{name}: {spacing}Optional[{type}]=None'.format(
+            name=self.target_entity_name_snek_case, spacing=_create_padding(padding, self.target_entity_name_snek_case),
+            type=self.target_entity_name
+        )
+
+    def to_type_constructor_body_string(self) -> str:
+        return '{name}={name} if owner else create_{name}(),'.format(
+            name=self.target_entity_name_snek_case
+        )
+
+    def to_sqlalchemy_type_constructor_string(self) -> str:
+        return '            {name}=self.{name}.to_{name}(),'.format(
+            name=self.target_entity_name_snek_case
+        )
+
+    def to_sqlalchemy_model_string(self, padding: int) -> str:
+        return "    {name} = {spacing}db.relationship('{class_name}', lazy={lazy}, uselist={uselist}{secondary})".format(
+            name=self.target_entity_name_snek_case,
+            class_name=self.target_entity_name,
+            uselist=str(self.join == JoinOption.to_one),
+            spacing=_create_padding(padding, self.target_entity_name_snek_case),
+            lazy=str(self.lazy),
+            secondary=', secondary={}'.format(self.join_table),
+        )
+
+    def to_named_tuple_string(self, padding: int):
+        return "        ('{name}', {spacing}{type}),".format(
+            name=self.target_entity_name_snek_case,
+            spacing=_create_padding(padding, self.target_entity_name_snek_case),
+            type=self.target_entity_name
+        )
 
 
 def create_relationship(
@@ -134,6 +216,7 @@ def create_relationship(
         nullable:           bool,
         lazy:               bool,
         join:               JoinOption,
+        join_table:         Optional[str]=None,
 ) -> Relationship:
     return Relationship(
         target_entity_name=target_entity_name,
@@ -141,23 +224,94 @@ def create_relationship(
         nullable=nullable,
         lazy=lazy,
         join=join,
+        join_table=join_table,
     )
 
 
 @attr.s
 class Entity(object):
-    class_name: str =                   attr.ib()
-    columns: List[Column] =             attr.ib()
-    class_name_snek_case: str =         attr.ib()
-    import_name: str =                  attr.ib()
-    column_length: int =                attr.ib()
-    relationships: List[Relationship] = attr.ib()
+    class_name:           str =                attr.ib()
+    columns:              List[Column] =       attr.ib()
+    class_name_snek_case: str =                attr.ib()
+    import_name:          str =                attr.ib()
+    column_length:        int =                attr.ib()
+    relationships:        List[Relationship] = attr.ib()
 
     def to_dict(self):
         d = self.__dict__
         d['columns'] = [c.to_dict() for c in self.columns]
         d['relationships'] = [r.to_dict() for r in self.relationships]
         return d
+
+    def to_type_constructor_string(self) -> str:
+        column_arguments = [c.to_type_constructor_argument_string(self.column_length) for c in self.columns]
+        relationship_arguments = [r.to_type_constructor_argument_string(self.column_length) for r in self.relationships]
+        constructor_args = '\n        '.join(column_arguments + relationship_arguments)
+        column_body = [c.to_type_constructor_body_string() for c in self.columns]
+        relationship_body = [r.to_type_constructor_body_string() for r in self.relationships]
+        body_args = '\n        '.join(column_body + relationship_body)
+        template = """
+def create_{entity_name}(
+        {constructor_args}
+) -> {class_name}:
+    return {class_name}(
+        {body_args}
+    )
+""".format(
+            constructor_args=constructor_args,
+            entity_name=self.class_name_snek_case,
+            body_args=body_args,
+            class_name=self.class_name,
+        )
+        return template
+
+    def to_sqlalchemy_model(self) -> str:
+        template = """
+class {class_name}(db.Model):  # type: ignore
+    id ={spacing}db.Column(db.Integer, primary_key=True)
+    {properties}{type_constructor}"""
+
+        columns = '\n    '.join(
+            [c.to_sqlalchemy_model_string(self.column_length) for c in self.columns]
+        ),
+        relationships = '\n    '.join(
+            [r.to_sqlalchemy_model_string(self.column_length) for r in self.relationships]
+        ),
+        return template.format(
+            class_name=self.class_name,
+            spacing=' ' * (self.column_length - 1),
+            properties='\n'.join(columns + relationships),
+            type_constructor=self.to_sqlalchemy_type_constructor(),
+        )
+
+    def to_sqlalchemy_type_constructor(self) -> str:
+        template =\
+            """
+    def to_{class_name}(self) -> {import_name}:
+        return create_{class_name}(
+            {properties}
+        )
+"""
+        tab = ' ' * 4
+        columns = ('\n' + tab * 3).join(
+            [c.to_sqlalchemy_type_constructor_string() for c in self.columns]
+        ),
+        relationships = ('\n' + tab * 3).join(
+            [r.to_sqlalchemy_type_constructor_string() for r in self.relationships]
+        ),
+        return template.format(
+            class_name=self.class_name_snek_case,
+            import_name=self.import_name,
+            properties='\n'.join(columns + relationships).rstrip(),
+        )
+
+    def to_type_string(self):
+        properties_join = '\n'
+        columns = properties_join.join([c.to_named_tuple_string(self.column_length) for c in self.columns])
+        relationships = properties_join.join([r.to_named_tuple_string(self.column_length) for r in self.relationships])
+        return "{class_name} = NamedTuple(\n    '{class_name}', [\n{columns}\n{relationships}    ]\n)".format(
+            class_name=self.class_name, columns=columns, relationships=relationships,
+        )
 
 
 def _camel_case_to_snek_case(x: str) -> str:
@@ -168,14 +322,18 @@ def _snek_case_to_camel_case(x: str) -> str:
     return inflection.camelize(x, False)
 
 
-def create_entity(class_name: str, columns: List[Column], relationships: List[Relationship]=[]) -> Entity:
+def create_entity(class_name: str, columns: List[Column], relationships: List[Relationship] = []) -> Entity:
     return Entity(
         class_name=class_name,
         class_name_snek_case=_camel_case_to_snek_case(class_name),
         import_name='{}Type'.format(class_name),
         columns=columns,
         # haha 🐍
-        column_length=(max(*[len(x) for x in [c.name for c in columns]])),
+        column_length=(max(*[
+            len(x) for x in
+            [c.name for c in columns] +
+            [r.target_entity_name_snek_case for r in relationships]]
+        )),
         relationships=relationships,
     )
 
@@ -187,8 +345,14 @@ def _entity_from_dict(entity_dict: Dict) -> Entity:
     )
 
 
-def _entity_from_exemplar(class_name: str, exemplar: Dict) -> Entity:
+def create_entity_from_exemplar(
+        class_name:   str,
+        exemplar:     Dict,
+        foreign_keys: List[Tuple[str, str]]=list(),
+        indexes:      List[str]=list(),
+) -> Entity:
     columns = []
+    foreign_keys_dict = dict(foreign_keys)
     for k, v in exemplar.items():
         if v is None:
             v = ''
@@ -196,96 +360,36 @@ def _entity_from_exemplar(class_name: str, exemplar: Dict) -> Entity:
         type_option = _string_to_type_option(type_name)
         if type_option == TypeOption.string and _is_string_date_time_ish(v):
             type_option = TypeOption.datetime
-        columns.append(create_column(k, type_option))
+        foreign_key = foreign_keys_dict[k] if k in foreign_keys_dict else None
+        index = k in indexes
+        columns.append(create_column(k, type_option, foreign_key, index))
     return create_entity(
         class_name=class_name,
         columns=columns,
     )
 
 
-db_model_template = """
-class {{ entity.class_name }}(db.Model):  # type: ignore
-{%- set padding = entity.column_length - ('id'|length) %}
-    id ={%for i in range(0, padding)%} {%endfor%} db.Column(db.Integer, primary_key=True)
-{%- for column in entity.columns %}
-{%- set padding = entity.column_length - (column['name']|length) %}
-    {{ column.name }} ={%for i in range(0, padding)%} {%endfor%} db.Column(db.{{ column.sql_alchemy_type.value }})
-{%- endfor %}
-{%- for relationship in entity.relationships %}
-{%- set padding = entity.column_length - (relationship.target_entity_name_snek_case|length) %}
-    {{ relationship.target_entity_name_snek_case }} = {%for i in range(0, padding)%} {%endfor%}db.relationship('{{ relationship.target_entity_name }}', lazy={{ "" ~ relationship.lazy }}{%if relationship.join.value == 'to_one'%}, uselist=False{%endif%})
-{%- endfor %}
-
-    def to_json(self) -> Dict:
-        d = {
-{%- for column in entity.columns %}
-{%- set padding = entity.column_length - (column['name']|length) %}
-            "{{ column.camel_case_name }}": {%for i in range(0, padding)%} {%endfor%}{{ column.python_type.value }}(self.{{ column.name }}),
-{%- endfor %}
-        }
-{%- for relationship in entity.relationships %}
-        d['{{ relationship.target_entity_name_snek_case }}'] = self.{{ relationship.target_entity_name_snek_case }}.to_{{ relationship.target_entity_name_snek_case }}()
-{%- endfor %}
-        return d
-"""
-
-named_tuple_template = """
-{{ entity.class_name }} = NamedTuple(
-    '{{ entity.class_name }}', [
-{%- for column in entity.columns %}
-{%- set padding = entity.column_length - (column.name|length) %}
-        ('{{ column.name }}', {%for i in range(0, padding)%} {%endfor%}{{ column.python_type.value }}),
-{%- endfor %}
-{%- for relationship in entity.relationships %}
-{%- set padding = entity.column_length - (relationship.target_entity_name_snek_case|length) %}
-        ('{{ relationship.target_entity_name_snek_case }}', {%for i in range(0, padding)%} {%endfor%}{{ relationship.target_entity_name_snek_case }}),
-{%- endfor %}
-    ]
-)
-"""
-
-type_constructor_template = """
-def create_{{ entity.class_name_snek_case }}(
-{%- for column in entity.columns %}
-{%- set padding = entity.column_length - (column.name|length) %}
-        {{ column.name }}: {%for i in range(0, padding)%} {%endfor%}Optional[{{ column.python_type.value }}]=None,
-{%- endfor %}
-{%- for relationship in entity.relationships %}
-{%- set padding = entity.column_length - (relationship.target_entity_name_snek_case|length) %}
-        {{ relationship.target_entity_name_snek_case }}: {%for i in range(0, padding)%} {%endfor%}Optional[{{ relationship.target_entity_name }}]=None
-{%- endfor %}
-) -> {{ entity.class_name }}:
-    return {{ entity.class_name }}(
-{%- for column in entity.columns %}
-{%- set padding = entity.column_length - (column.name|length) %}
-        {{ column.name }}={{ column.name }} if {{ column.name }} else {{ column.default }},
-{%- endfor %}
-{%- for relationship in entity.relationships %}
-        {{ relationship.target_entity_name_snek_case }}={{ relationship.target_entity_name_snek_case }} if {{ relationship.target_entity_name_snek_case }} else create_{{ relationship.target_entity_name_snek_case }}(), 
-{%- endfor %}
+def render_db_model(entity: Entity, db_import: str, types_module: str):
+    return '{types_import}\n{db_import}\n\n{entity}'.format(
+        db_import=db_import,
+        entity=entity.to_sqlalchemy_model(),
+        types_import='from {module} import {entity_type} as {entity_type}Type, {create_entity}'.format(
+            module=types_module, entity_type=entity.class_name,
+            create_entity='create_{}'.format(entity.class_name_snek_case)
+        ),
     )
-"""
-
-to_type_template = """
-    def to_{{ entity.class_name_snek_case }}(self) -> {{ entity.import_name }}:
-        return create_{{ entity.class_name_snek_case }}(
-{%- for column in entity.columns %}
-            {{ column.name }}={{ column.python_type.value }}(self.{{ column.name }}),
-{%- endfor %}
-{%- for relationship in entity.relationships %}
-            {{ relationship.target_entity_name_snek_case }}=self.{{ relationship.target_entity_name_snek_case }}.to_{{ relationship.target_entity_name_snek_case }}(), 
-{%- endfor %}
-        )
-"""
 
 
-def render_templates(entity: Entity) -> Tuple[str, str, str, str]:
-    db_jinja_template = Environment(loader=BaseLoader).from_string(db_model_template)
-    db_rendered = db_jinja_template.render({"entity": entity.to_dict()})
-    tuple_jinja_template = Environment(loader=BaseLoader).from_string(named_tuple_template)
-    tuple_rendered = tuple_jinja_template.render({"entity": entity})
-    to_type_jinja_template = Environment(loader=BaseLoader).from_string(to_type_template)
-    to_type_rendered = to_type_jinja_template.render({"entity": entity})
-    type_constructor_jinja_template = Environment(loader=BaseLoader).from_string(type_constructor_template)
-    type_constructor_rendered = type_constructor_jinja_template.render({"entity": entity})
-    return db_rendered, tuple_rendered, to_type_rendered, type_constructor_rendered
+def render_type_model(entity: Entity):
+    return """from typing import NamedTuple, Optional
+
+{named_tuple}
+
+{tuple_constructor}""".format(
+        named_tuple=entity.to_type_string(),
+        tuple_constructor=entity.to_type_constructor_string(),
+    )
+
+
+def render_type_constructor(entity: Entity):
+    return entity.to_type_constructor_string()
