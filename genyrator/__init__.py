b75d54be2e3e2ef6965fb5f3dfcbc716a6054b9c
jumblesale <rob@robsollars.co.uk>
2018-05-24 10:30:03 +0100

    added an init to make imports ~nicer~

---
+++ genyrator/__init__.py
@@ -0,0 +1,14 @@
+from genyrator.genyrator import (
+    # methods
+    create_entity,
+    create_entity_from_exemplar,
+    create_relationship,
+    render_type_model,
+    render_db_model,
+    render_type_constructor,
+
+    # classes
+    Entity,
+    Relationship,
+    JoinOption,
+)
