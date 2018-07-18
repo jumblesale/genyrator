007fe3989f328960bd77c08ecf5a43964327e5df
jumblesale <rob@robsollars.co.uk>
2018-07-18 11:15:59 +0100

    added a before_feature step to empty out the output directory

---
+++ test/e2e/environment.py
@@ -0,0 +1,20 @@
+import os
+import glob
+import shutil
+
+from genyrator.path import get_root_path_list
+
+
+def before_feature(context, feature):
+    """
+    delete the contents of the output dir before every feature gets run
+    :param context:
+    :param feature:
+    :return:
+    """
+    root_path = get_root_path_list()
+    # make very sure this is the directory which the command is being run in
+    files = glob.glob(os.path.join(*root_path, 'output', '*'))
+    for f in files:
+        # shutil.rmtree(f)
+        ...
