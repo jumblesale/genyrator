bac3de0c1bca46ead4af7a6c902226b9378de0a9
Chris Gearing <chrisagearing@outlook.com>
2018-08-08 13:28:40 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,10 @@ from setuptools import setup
 setup(
     name='genyrator',
     version='0.0.2',
-    packages=['genyrator'],
+    packages=find_packages(exclude=['tests']),
     url='https://github.com/jumblesale/genyrator',
     license='',
     author='jumblesale',
     author_email='',
     description='',
-    package_dir={'' : 'genyrator'},
 )
