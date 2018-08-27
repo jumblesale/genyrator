1fc9fa3bed5f15f9e52a255a97e8b888ce962520
jumblesale <rob@robsollars.co.uk>
2018-08-27 09:20:32 +0100

    added (passing) specs for model_to_dict for the case where relationships are not present

--- bookshop/resources/Book.py
+++ bookshop/resources/Book.py
@@ -103,39 +103,11 @@ class BookResource(Resource):  # type: ignore
         if result is None:
             abort(404)
 
-        result_dict = model_to_dict(
-            result,
-            book_domain_model,
-        )
+        python_dict = json_dict_to_python_dict(data)
+        [setattr(result, k, v) for k, v in python_dict.items()]
 
-        data = {**result_dict, **data}
-
-        joined_entities = create_joined_entity_map(
-            book_domain_model,
-            data,
-        )
-
-        data = convert_properties_to_sqlalchemy_properties(
-            book_domain_model,
-            joined_entities,
-            json_dict_to_python_dict(data),
-        )
-
-        marshmallow_result = book_schema.load(
-            data,
-            session=db.session,
-            instance=result,
-        )
-        if marshmallow_result.errors:
-            abort(400, python_dict_to_json_dict(marshmallow_result.errors))
-
-        db.session.add(marshmallow_result.data)
+        db.session.add(result)
         db.session.commit()
-
-        return model_to_dict(
-            marshmallow_result.data,
-            book_domain_model,
-        ), 200
 
 
 @api.route('/books', endpoint='books')  # noqa: E501
--- test/spec/bookshop/sqlalchemy/model_to_dict_spec.py
+++ test/spec/bookshop/sqlalchemy/model_to_dict_spec.py
@@ -1,27 +1,43 @@
+import datetime
 import uuid
 
-from expects import expect, have_keys
+from expects import expect, have_keys, have_key, equal
 from mamba import description, it
 from sqlalchemy.orm import joinedload
 
 from bookshop import app
 from bookshop.sqlalchemy.model import Book, Author
+from bookshop.domain.Book import book as book_domain_model
 from bookshop.sqlalchemy.model_to_dict import model_to_dict
 from bookshop import db
 
-BOOK_ID =   uuid.uuid4()
-AUTHOR_ID = uuid.uuid4()
+BOOK_UUID =   uuid.uuid4()
+AUTHOR_UUID = uuid.uuid4()
 
-author_model = Author(author_id=str(AUTHOR_ID), name='orwell')
+datetime_now = datetime.datetime.now()
+date_now =     datetime.datetime.today()
+
+author_model = Author(id=1, author_id=str(AUTHOR_UUID), name='orwell')
 author_dict = {
-    "authorId": str(AUTHOR_ID),
+    "authorId": str(AUTHOR_UUID),
     "name": 'orwell',
 }
-book_model = Book(book_id=str(BOOK_ID), name='animal farm', rating=4.1, author_id=str(AUTHOR_ID))
+book_model = Book(
+    id=1,
+    book_id=str(BOOK_UUID),
+    name='animal farm',
+    rating=4.1,
+    author_id=1,
+    published=date_now,
+    created=datetime_now,
+)
+
 book_dict = {
-    "bookId": str(BOOK_ID),
+    "bookId": str(BOOK_UUID),
     "name": 'animal farm',
     "rating": 4.1,
+    "published": date_now.strftime('%Y-%m-%d'),
+    "created": datetime_now.isoformat(),
 }
 
 with app.app_context():
@@ -34,16 +50,43 @@ with app.app_context():
 with description('model_to_dict') as self:
     with it('converts a flat model into a dict'):
         with app.app_context():
-            retrieved_book = Book.query.filter_by(book_id=BOOK_ID).first()
-        result = model_to_dict(model=retrieved_book)
+            retrieved_book = Book.query.filter_by(book_id=BOOK_UUID).first()
+        result = model_to_dict(
+            sql_alchemy_model=retrieved_book,
+            domain_model=book_domain_model,
+        )
         expect(result).to(have_keys(**book_dict))
 
     with it('converts a singly-nested relationship'):
         with app.app_context():
             retrieved_book = Book.query.\
-                filter_by(book_id=BOOK_ID).\
+                filter_by(book_id=BOOK_UUID).\
                 options(joinedload('author')).\
                 first()
-        result = model_to_dict(model=retrieved_book, paths=['author'])
+        result = model_to_dict(
+            sql_alchemy_model=retrieved_book,
+            domain_model=book_domain_model,
+            paths=['author'],
+        )
         expect(result).to(have_keys(**book_dict))
         expect(result['author']).to(have_keys(**author_dict))
+
+    with it('gives an empty response when relationship does not exist'):
+        book_without_author = Book(
+            book_id=str(uuid.uuid4()),
+            name='',
+            rating=0.2,
+        )
+        with app.app_context():
+            db.session.add(book_without_author)
+            db.session.commit()
+            retrieved_book = Book.query.\
+                filter_by(book_id=book_without_author.book_id).\
+                first()
+        result = model_to_dict(
+            sql_alchemy_model=retrieved_book,
+            domain_model=book_domain_model,
+            paths=['author'],
+        )
+        expect(result).to(have_key('author'))
+        expect(result['author']).to(equal(None))
