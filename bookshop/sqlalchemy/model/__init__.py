a97b9f182a92f9e1da22df9982d5c84ff547595c
Chris Gearing <gearichr@turntown.com>
2018-10-25 14:45:40 +0100

    Actually changing the eager loading formatting

--- bookshop/sqlalchemy/model/__init__.py
+++ bookshop/sqlalchemy/model/__init__.py
@@ -1,7 +1,7 @@
 # flake8: noqa
-from bookshop.sqlalchemy.model.Book import Book
 from bookshop.sqlalchemy.model.Author import Author
-from bookshop.sqlalchemy.model.Review import Review
-from bookshop.sqlalchemy.model.Genre import Genre
+from bookshop.sqlalchemy.model.Book import Book
 from bookshop.sqlalchemy.model.BookGenre import BookGenre
+from bookshop.sqlalchemy.model.Genre import Genre
+from bookshop.sqlalchemy.model.Review import Review
 
