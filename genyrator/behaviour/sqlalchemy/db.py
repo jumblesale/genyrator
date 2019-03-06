d9014b78af54c1628140d265d624828ab397fd7d
jumblesale <rob@robsollars.co.uk>
2019-03-06 09:42:08 +0100

    added test for trying to use methods before db has been initialized

--- genyrator/behaviour/sqlalchemy/db.py
+++ genyrator/behaviour/sqlalchemy/db.py
@@ -3,15 +3,20 @@ from functools import lru_cache
 from flask_sqlalchemy import SQLAlchemy
 
 
+class DBNotInitialisedException(Exception):
+    message = "It looks like you're trying to use genyrator without having "
+    "initialised the db object first! Call `init_genyrator` with your SQLAlchemy "
+    "db instance before trying to use any of the generated code."
+
+
 def init_genyrator(
     sqlalchemy_instance: SQLAlchemy,
 ):
     get_db_instance._db_instance = sqlalchemy_instance
 
 
-@lru_cache()
 def get_db_instance():
-    if get_db_instance._db_instance is None:
-        ...
+    if not hasattr(get_db_instance, '_db_instance'):
+        raise DBNotInitialisedException(DBNotInitialisedException.message)
     else:
         return get_db_instance._db_instance
--- test/spec/genyrator/behaviour/sqlalchemy/db_spec.py
+++ test/spec/genyrator/behaviour/sqlalchemy/db_spec.py
@@ -1,15 +1,24 @@
-from expects import be_a, expect
+from expects import be_a, expect, be_none
 from flask_sqlalchemy import SQLAlchemy
 from mamba import description, it
 
 from genyrator.behaviour.sqlalchemy.db import (
-    init_genyrator, get_db_instance
-)
+    init_genyrator, get_db_instance,
+    DBNotInitialisedException)
 
 from bookshop.sqlalchemy import db as bookshop_db
 
 
 with description('using the db instance'):
+    with it('throws if the db has not been initialized'):
+        exception = None
+        try:
+            instance = get_db_instance()
+        except DBNotInitialisedException as e:
+            exception = e
+        expect(exception).to_not(be_none)
+
+
     with it('provides access to the db instance'):
         init_genyrator(sqlalchemy_instance=bookshop_db)
         instance = get_db_instance()
